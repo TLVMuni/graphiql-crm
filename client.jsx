87b625a75e7713d3d14e77ddc5d9223e0916a603
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-27 02:45:09 +0200

    Update client.jsx

--- client.jsx
+++ client.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
+
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers);
+
+ReactDOM.render(<Provider store={store}>
+                  <App />
+                </Provider>, document.getElementById('root'));
