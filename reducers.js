971db5766a4c7e28fc80f1c91c777368d7fa02cb
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-27 13:06:38 +0200

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 const INITIAL_STATE = {
-  loggedIn: false
+  loggedIn: false,
+  jwt: {}
 };
 
 const reducers = (state = INITIAL_STATE, action) => {
@@ -9,7 +10,10 @@ const reducers = (state = INITIAL_STATE, action) => {
   switch( action.type ) {
 
     case 'LOGGED_IN':
-      state = _.assign({}, state, {loggedIn: action.data});
+      state = _.assign({}, state, {
+                                    loggedIn: action.data.status,
+                                    authHeader: action.data.authHeader
+                                  });
       break;
 
     default:
